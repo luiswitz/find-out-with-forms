8b9a3355123ef1b9645a6bc57b360d694aaf08ea
Luis Henrique Witz <luiswitz@gmail.com>
2018-01-18 22:29:32 -0200

    User model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    let(:user) { create(:user) }
+    let(:form) { create(:form, user: user) }
+
+    it 'has many forms' do
+      association = described_class.reflect_on_association(:forms)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'deletes related forms' do
+      user.delete 
+      expect(Form.where(user_id: user.id).count).to eq 0
+    end
+  end
+end
