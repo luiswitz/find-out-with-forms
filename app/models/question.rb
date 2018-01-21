56d2724c3882856143117b31ab694e0011f647d5
Luis Henrique Witz <luiswitz@gmail.com>
2018-01-21 19:35:24 -0200

    Add validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :form
   has_many :questions_answers, dependent: :destroy
+  
+  validates :title, :kind, :form,  presence: true
+  enum kind: [:short_text, :long_text, :integer, :boolean]
 end
