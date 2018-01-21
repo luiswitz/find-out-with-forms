f24f9e4c244384da3b2392c6c92ea219d9818159
Luis Henrique Witz <luiswitz@gmail.com>
2018-01-21 19:22:46 -0200

    Associations tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'asssociations' do
+    let!(:question) { create(:question) }
+    let!(:question_answear) { create(:questions_answer, question: question) }
+
+    it 'belongs to a form' do
+      association = described_class.reflect_on_association(:form)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'has many questions_answears' do
+      association = described_class.reflect_on_association(:questions_answers)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'deletes all related questions_answears' do
+      question.destroy
+      expect(QuestionsAnswer.where(question_id: question.id).count).to eq 0
+    end
+  end
 end
