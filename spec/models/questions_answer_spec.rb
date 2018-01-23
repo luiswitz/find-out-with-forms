f6bd9c81207ccb569606658b9afd0a899a49f61f
Luis Henrique Witz <luiswitz@gmail.com>
2018-01-23 20:40:32 -0200

    Add validations tests for question_answer model

--- spec/models/questions_answer_spec.rb
+++ spec/models/questions_answer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsAnswer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'belongs to an answer' do
+      association = described_class.reflect_on_association(:answer)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+
+    it 'belongs to a question' do
+      association = described_class.reflect_on_association(:question)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
 end
+
