ebb712968976cc7afa50c3fce640c45508a2c6ea
Luis Henrique Witz <luiswitz@gmail.com>
2018-02-23 14:44:49 -0300

    Questions api create, WIP

--- spec/requests/api/v1/api_v1_questions_spec.rb
+++ spec/requests/api/v1/api_v1_questions_spec.rb
@@ -1,10 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Questions", type: :request do
-  describe "GET /api_v1_questions" do
-    it "works! (now write some real specs)" do
-      get api_v1_questions_index_path
-      expect(response).to have_http_status(200)
+  let(:user) { create(:user) }
+  let(:form) { create(:form, user: user) }
+
+  describe '#create' do
+    context 'deny without authentication headers' do
+      it_behaves_like :deny_without_authorization, :post, '/api/v1/questions'
+    end
+
+    context 'with valid parameters' do
+      let(:question_attributes) { attributes_for(:question) }
+
+      before do
+        post '/api/v1/questions',
+          params: { question: question_attributes, form_id: form.id },
+          headers: header_with_authentication(user)
+      end
+
+      it 'return http code 200' do
+        expect_status(200)
+      end
+
+      it 'is created with valid data' do
+        question_attributes.each do |field|
+          expect(Question.first[field.first.to_s]).to eq(field.last)
+        end
+      end
+
+      it 'returns the correct data' do
+        question_attributes.each do |field|
+          expect(json[field.first.to_s]).to eq(field.last)
+        end
+      end
+    end
+
+    context 'with invalid parameters' do
+      context 'valid form' do
+        it 'returns http code 400' do
+          post '/api/v1/questions',
+            params: { question: {}, form_id: form.id },
+            headers: header_with_authentication(user)
+
+          expect_status(400)
+        end
+      end
+
+      context 'invalid form' do
+        it 'returns http code 404' do
+          post '/api/v1/questions',
+            params: { question: {} },
+            headers: header_with_authentication(user)
+          
+          expect_status(404)
+        end
+      end
     end
   end
 end
